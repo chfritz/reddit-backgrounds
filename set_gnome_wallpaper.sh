eb07bd24ef86c8c54680d0ed186daec8234e7fe8
Christian Fritz <christian.fritz@gmail.com>
2022-04-18 20:55:18 -0700

    Fixes a small bug when there are multiple gnome-sessions
    - caused setting the wallpaper to fail, because it could find the wrong session in pgrep. Now using current user's id to filter.

--- set_gnome_wallpaper.sh
+++ set_gnome_wallpaper.sh
@@ -4,7 +4,7 @@
 # set up for the desktop
 
 # From https://stackoverflow.com/questions/10374520/gsettings-with-cron
-PID=$(pgrep gnome-session | head -n 1)
+PID=$(pgrep -U $(id -u) gnome-session | head -n 1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 echo "setting wallpaper to $1"
